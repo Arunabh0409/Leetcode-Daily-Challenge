b7f53359b62e0e7889c4dc39512cefc7f15bc80d
Arunabh0409 <arunabhagarwalindia@gmail.com>
2024-04-02 18:01:09 +0530

    First code Push

--- March/01.cpp
+++ March/01.cpp
@@ -1,12 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLastWord(string s) {
-        int ans=0;
-        int i=s.length()-1;
-        while(s[i]==' ')i--;
-        for(;i>=0;i--){
-            if(s[i]!=' ')ans++;
-            else return ans;
-        }return ans;
+    int lengthOfLastWord(string s)
+    {
+        int ans = 0;
+        int i = s.length() - 1;
+        while (s[i] == ' ')
+            i--;
+        for (; i >= 0; i--)
+        {
+            if (s[i] != ' ')
+                ans++;
+            else
+                return ans;
+        }
+        return ans;
     }
 };
